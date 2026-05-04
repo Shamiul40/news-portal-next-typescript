3804c26a804090031a5499c31abe956d9446ecfd
Shamiul40 <shamiul4040@gmail.com>
2026-05-04 23:00:22 +0600

    navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -16,15 +16,37 @@ export default function Navbar() {
 
       {/* items */}
 
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem> 
-           <NavigationMenuLink href="/news">News</NavigationMenuLink>
+      <NavigationMenu >
+        <NavigationMenuList className="flex gap-4" >
+          {/* news*/}
+          <NavigationMenuItem >
+            <NavigationMenuLink  href="/news">News</NavigationMenuLink>
+          </NavigationMenuItem>
+          {/* services */}
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Services</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Web Development</NavigationMenuLink>
+              <NavigationMenuLink>Mobile Apps</NavigationMenuLink>
+              <NavigationMenuLink>Graphic Design</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          {/* about */}
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/about">About</NavigationMenuLink>
+          </NavigationMenuItem>
+          {/* contact */}
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/contact">Contact</NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
 
       {/* login */}
+      <div className="flex">
+        <h1>Theme</h1>
+      <h1>Login</h1>
+      </div>
     </header>
   );
 }
