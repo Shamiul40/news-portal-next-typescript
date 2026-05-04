f33da47e3d414e16edf407b33b7ddc4cdaa46e83
Shamiul40 <shamiul4040@gmail.com>
2026-05-05 00:24:47 +0600

    navbar complete

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -9,16 +9,17 @@ import {
 } from "../ui/navigation-menu";
 import { Switch } from "../ui/switch";
 import { Button } from "../ui/button";
+import { Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between px-4 shadow-md my-2">
+    <header className="flex justify-between items-center px-4 shadow-md my-2">
       {/* logo */}
       <Link href="/">Daily News</Link>
 
       {/* items */}
 
-      <NavigationMenu>
+      <NavigationMenu className="hidden lg:flex">
         <NavigationMenuList className="flex gap-4">
           {/* news*/}
           <NavigationMenuItem>
@@ -45,13 +46,14 @@ export default function Navbar() {
       </NavigationMenu>
 
       {/* login and dark mode */}
-      <div className="flex gap-4 justify-center">
+      <div className="flex gap-4 justify-center hidden lg:flex">
         <div className="flex gap-2 items-center">
           <span>Dark Mode</span>
           <Switch />
         </div>
         <Button className="cursor-pointer" variant="default">Button</Button>
       </div>
+      <Button className="lg:hidden"><Menu size={24} /></Button>
     </header>
   );
 }
