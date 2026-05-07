5b8f5d8209481cf89552042382d30e96f5f9b3e4
Shamiul40 <shamiul4040@gmail.com>
2026-05-07 20:20:53 +0600

    tech page added

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -10,6 +10,7 @@ export default function NewsLayout({
       <div className="space-x-3 py-8">
         <Link href="/news/sports">Sports</Link>
         <Link href="/news/health">Health</Link>
+        <Link href="/news/tech">Technology</Link>
       </div>
       <div className="flex justify-between mx-auto">
         <div className="flex justify-center items-center w-8/12 h-48 bg-gray-300">
